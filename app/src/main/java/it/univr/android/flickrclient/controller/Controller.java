b354417ab3ba99a90b4c7bf459ddccd45ad9ff95
Sergio Alberti <parallelosec@gmail.com>
2017-06-30 17:08:44 +0200

    Move ThumbTask from AsyncTask to Executors and CompletionService
    AsyncTask must be invoked on the UiThread for short operations
    (this is not the case)

--- app/src/main/java/it/univr/android/flickrclient/controller/Controller.java
+++ app/src/main/java/it/univr/android/flickrclient/controller/Controller.java
@@ -9,6 +9,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Interpolator;
 import android.os.AsyncTask;
+import android.renderscript.ScriptGroup;
 import android.support.annotation.UiThread;
 import android.support.annotation.WorkerThread;
 import android.util.Log;
@@ -23,12 +24,21 @@ import it.univr.android.flickrclient.model.Model;
 import it.univr.android.flickrclient.view.View;
 
 import java.util.ArrayList;
-
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Controller {
     private final String TAG = Controller.class.getName();
     private MVC mvc;
-    private final ArrayList<ThumbTask> thumbTaskList = new ArrayList<>();
+    private ArrayList<Future<Model.FlickrImage>> thumbTaskList = new ArrayList<>();
+    private static ExecutorService executor = null;
+
 
     @UiThread
     public void setMVC(MVC mvc){
@@ -43,17 +53,33 @@ public class Controller {
 
     //@UiThread
     public void callThumbTask(ArrayList<Model.FlickrImage> images) {
+        executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        CompletionService<Model.FlickrImage> completionService =
+                new ExecutorCompletionService<>(executor);
+
         for(Model.FlickrImage image : images)
-            thumbTaskList.add((ThumbTask) new ThumbTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, image));
+            completionService.submit(new ThumbTask(image));
+
+        try{
+            for(int i=0; i<images.size(); i++) {
+                Future<Model.FlickrImage> f = completionService.take();
+                thumbTaskList.add(f);
+                mvc.model.updateImage(f.get());
+            }
+        }
+        catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+        catch(ExecutionException e){
+            Log.d("THUMB TASK", "execution exception");
+        }
     }
 
     //@WorkerThread
     public void killWorkingTasks(){
-        if(!thumbTaskList.isEmpty()){
-            for(ThumbTask t : thumbTaskList)
-                t.cancel(true);
-
-            thumbTaskList.clear();
+        if(executor != null) {
+            executor.shutdownNow();
+            executor = null;
         }
     }
 
@@ -65,13 +91,19 @@ public class Controller {
         mvc.forEachView(View::clearPreviousSearch);
     }
 
-    private class ThumbTask extends AsyncTask<Model.FlickrImage, Void, Model.FlickrImage> {
 
-        @Override @WorkerThread
-        protected Model.FlickrImage doInBackground(Model.FlickrImage... image) {
-            if(!isCancelled()) {
-                try {
-                    String CONNECTION_URL = image[0].getThumbURL();
+    private class ThumbTask implements Callable<Model.FlickrImage> {
+        private final Model.FlickrImage image;
+
+        public ThumbTask(Model.FlickrImage image){
+            this.image = image;
+        }
+
+        @Override
+        public Model.FlickrImage call() {
+            try {
+                if(!executor.isShutdown()) {
+                    String CONNECTION_URL = image.getThumbURL();
                     URL url = new URL(CONNECTION_URL);
                     URLConnection connection = url.openConnection();
                     connection.setDoInput(true);
@@ -80,19 +112,14 @@ public class Controller {
                     InputStream input = connection.getInputStream();
                     Bitmap thumb = BitmapFactory.decodeStream(input);
 
-                    return image[0].setThumbBitmap(thumb);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    return null;
+                    image.setThumbBitmap(thumb);
                 }
             }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
 
-            return null;
-        }
-
-        @Override @UiThread
-        protected void onPostExecute(Model.FlickrImage im) {
-            mvc.model.updateImage(im);
+            return image;
         }
 
     }
